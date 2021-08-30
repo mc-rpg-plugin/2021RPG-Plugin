4ebef474c8308b5dbd9b25524ce5ef44a79ac3bf
krapoi <hjn2006@naver.com>
2021-08-30 15:53:57 +0900

    1

--- src/com/tistory/workshop/Berserker.java
+++ src/com/tistory/workshop/Berserker.java
@@ -1,22 +1,28 @@
 package com.tistory.workshop;
 
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 public class Berserker implements Listener {
     @EventHandler
-    public void bsk(PlayerInteractEvent e){
-        Player p = e.getPlayer();
-        if(p.getHealth() <= 10){
-            p.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE,600,1));
-        }else if(p.getHealth() <= 7){
-            p.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE,600,2));
-        }else if(p.getHealth() <= 5){
-            p.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE,600,4));
+    public void bsk(EntityDamageEvent e){
+        if(e.getEntityType() != EntityType.PLAYER)
+            return;
+
+        Player p = (Player) e.getEntity();
+        if(p.getHealth() - e.getDamage() <= 10){
+            p.removePotionEffect(PotionEffectType.INCREASE_DAMAGE);
+            p.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE,1000000000,1));
+        }else if(p.getHealth() - e.getDamage() <= 7 && p.getHealth() > 5 ){
+            p.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE,1000000000,2));
+        }else if(p.getHealth() - e.getDamage()<= 5){
+            p.removePotionEffect(PotionEffectType.INCREASE_DAMAGE);
+            p.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE,1000000000,4));
         }
     }
 }
