9c126dd0b44785cbf72ca45e2d54185b4e496553
iqpizza6349 <iqpizza6349@gmail.com>
2021-08-31 08:34:24 +0900

    create book and clickable text (can run command)

--- src/com/tistory/workshop/command/RPGCommand.java
+++ src/com/tistory/workshop/command/RPGCommand.java
@@ -12,24 +12,24 @@ public class RPGCommand implements CommandExecutor {
 
         if (!(sender instanceof Player)) return false;
 
-        Player player = (Player) sender;
+            Player player = (Player) sender;
 
-        if (strings.length == 0) {
-            // TODO help 명령어 사용하게 추후 개발
-        }
-        else {
-            if (strings[0].equals("setCharacter")) {
-                if (strings.length >= 2) {
-                    if (strings[1].equals("Fighter")) {
-                        sender.sendMessage("파이터 클래스로 설정하였습니다.");
+            if (strings.length == 0) {
+                // TODO help 명령어 사용하게 추후 개발
+            }
+            else {
+                if (strings[0].equals("setCharacter")) {
+                    if (strings.length >= 2) {
+                        if (strings[1].equals("Fighter")) {
+                            sender.sendMessage("파이터 클래스로 설정하였습니다.");
+                        }
+                    }
+                    else {
+                        sender.sendMessage("틀린 명령어");   // TODO 추후 빨간색 텍스트로 에러 문자열 전송
                     }
                 }
-                else {
-                    sender.sendMessage("틀린 명령어");   // TODO 추후 빨간색 텍스트로 에러 문자열 전송
-                }
             }
-        }
 
-        return true;
+            return true;
     }
 }
