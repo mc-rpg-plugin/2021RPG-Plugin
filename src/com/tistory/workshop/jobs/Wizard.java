90136529cc3fe76e8e885ac8578b37b404aed34c
iqpizza6349 <iqpizza6349@gmail.com>
2021-09-04 18:02:23 +0900

    create jobs(classes) and make their abilities

--- src/com/tistory/workshop/jobs/Wizard.java
+++ src/com/tistory/workshop/jobs/Wizard.java
@@ -2,11 +2,15 @@ package com.tistory.workshop.jobs;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Particle;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+
+import java.util.ArrayList;
 
 public class Wizard implements Listener {
 
@@ -26,5 +30,33 @@ public class Wizard implements Listener {
             targetLocation.setYaw(yaw);
             player.teleport(targetLocation);
         }
+
+        if ((action == Action.LEFT_CLICK_AIR || action == Action.LEFT_CLICK_BLOCK) && player.getInventory().getItemInMainHand().getType() == Material.BLAZE_ROD) {
+            for (Block block : getBlocks(e.getClickedBlock(), 4)) {
+                if (block == null)
+                    continue;
+                player.getWorld().spawnParticle(Particle.DAMAGE_INDICATOR, block.getLocation(), 10);
+            }
+        }
     }
+
+    public ArrayList<Block> getBlocks(Block start, int radius) {
+        ArrayList<Block> blocks = new ArrayList<Block>();
+        if (start == null) {
+            blocks.add(null);
+            return blocks;
+        }
+
+        for(double x = start.getLocation().getX() - radius; x <= start.getLocation().getX() + radius; x++){
+            for(double y = start.getLocation().getY() - radius; y <= start.getLocation().getY() + radius; y++){
+                for(double z = start.getLocation().getZ() - radius; z <= start.getLocation().getZ() + radius; z++){
+                    Location loc = new Location(start.getWorld(), x, y, z);
+                    blocks.add(loc.getBlock());
+                }
+            }
+        }
+        return blocks;
+    }
+
+
 }
